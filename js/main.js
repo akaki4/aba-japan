2cdec51fec1146965362bc1bebff06a4f984c688
takumi <akaranki4@gmail.com>
2021-09-15 12:39:40 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+  // // スライダーの処理
+  // $('.slider').slick({
+  //   autoplay: true,
+  //   autoplaySpeed:5000,
+  //   dots:true,
+  //   centerMode: true,
+  //   centerPadding: '10%',
+  //   slidesToScroll: 1,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1080,
+  //       settings: {
+  //         centerMode: false,
+  //         centerPadding: '0%',
+  //       }
+  //     },
+  //   ]
+  // });
+  // // ハンバーガーメニューの処理
+  // $('.sp-hamburger-btn').on('click', function(){
+  //   $('.sp-hamburger-menu').toggleClass('close');
+  //   $('#sp-hamburger-list').toggleClass('open');
+  // });
+  // $(".hamburger-section").on("click", function(){
+  //   $('.sp-hamburger-menu').toggleClass('close');
+  //   $('#sp-hamburger-list').toggleClass('open');
+  // });
+});
